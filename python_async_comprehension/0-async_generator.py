40a870b8991819fb62c74e1b368bf1f22bafa897
Eziknmad <galarzaarzon@gmail.com>
2026-04-12 13:36:16 -0400

    Python - Async Comprehension task 0 fix

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """Module for async_generator coroutine."""
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Coroutine that loops 10 times, waits 1 second, then yields a random
     number between 0 and 10."""
     for _ in range(10):
